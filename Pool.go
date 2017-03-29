f519054f55c1dd16ecce4a24191b129ded350c78
Jacky Chiu <jacky.chiu888@gmail.com>
2017-03-28 23:26:39 -0400

    Implemented the heap interface

--- Pool.go
+++ Pool.go
@@ -1,3 +1,27 @@
 package main
 
 type Pool []*Worker
+
+func (p Pool) Len() int {
+	return len(p)
+}
+
+func (p Pool) Less(i, j int) bool {
+	return p[i].pending < p[j].pending
+}
+
+func (p Pool) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+}
+
+func (p *Pool) Push(x interface{}) {
+	*p = append(*p, x.(*Worker))
+}
+
+func (p *Pool) Pop() interface{} {
+	prev := *p
+	last := len(prev) - 1
+	elem := prev[last]
+	*p = prev[:last]
+	return elem
+}
