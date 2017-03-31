d6f1023f0452d944ff6354d54e0af79175048c56
Jacky Chiu <jacky.chiu888@gmail.com>
2017-03-30 20:01:10 -0400

    Created requester

--- Request.go
+++ Request.go
@@ -1,6 +1,23 @@
 package main
 
+import "time"
+
 type Request struct {
 	job    func() int
 	result chan int
+}
+
+func requester(work chan<- Request) {
+	result := make(chan int)
+	for {
+		// TODO: Make random sleep
+		time.Sleep(2 * time.Second)
+		work <- Request{job, result}
+		<-result
+	}
+}
+
+func job() int {
+	time.Sleep(1 * time.Second)
+	return 1
 }
