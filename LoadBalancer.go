d836af0aa5d61aca7055a39a64b07e521e29ea42
Jacky Chiu <jacky.chiu888@gmail.com>
2017-03-30 20:00:58 -0400

    Implimented methods for balancer

--- LoadBalancer.go
+++ LoadBalancer.go
@@ -1,6 +1,31 @@
 package main
 
+import "container/heap"
+
 type Balancer struct {
 	pool Pool
 	done chan *Worker
+}
+
+func (b *Balancer) Balance(work <-chan Request) {
+	for {
+		select {
+		case req := <-work:
+			b.dispatch(req)
+		case worker := <-b.done:
+			b.complete(worker)
+		}
+	}
+}
+
+func (b *Balancer) dispatch(req Request) {
+	w := heap.Pop(&b.pool).(*Worker)
+	w.requests <- req
+	w.pending += 1
+	heap.Push(&b.pool, w)
+}
+
+func (b Balancer) complete(worker *Worker) {
+	worker.pending -= 1
+	heap.Fix(&b.pool, worker.index)
 }
