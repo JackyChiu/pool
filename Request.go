dcb36bc424051d1c5ff161555f8eb5ecaf5bc00f
Jacky Chiu <jacky.chiu888@gmail.com>
2017-04-01 15:55:31 -0400

    Wait for result in goroutine, New random duration for requests made and jobs

--- Request.go
+++ Request.go
@@ -10,18 +10,20 @@ type Request struct {
 	result chan int
 }
 
+func job() int {
+	randDuration := time.Duration(rand.Intn(4)) * time.Second
+	time.Sleep(randDuration + time.Second)
+	return 1
+}
+
 func requester(requests chan<- Request) {
 	result := make(chan int)
 	for {
-		randDuration := time.Duration(rand.Intn(1))
-		time.Sleep(randDuration * time.Second)
-		requests <- Request{job, result}
-		<-result
+		randDuration := time.Duration(rand.Intn(1)) * time.Second
+		time.Sleep(randDuration + 250*time.Millisecond)
+		go func() {
+			requests <- Request{job, result}
+			<-result
+		}()
 	}
-}
-
-func job() int {
-	randDuration := time.Duration(rand.Intn(3))
-	time.Sleep(randDuration * 5 * time.Second)
-	return 1
 }
