505446f459a61105251fc153cbb60e958c1907ca
Jacky Chiu <jacky.chiu888@gmail.com>
2017-04-01 11:48:12 -0400

    Added inital setup for system

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"container/heap"
+	"fmt"
+	"runtime"
+)
 
 func main() {
-	fmt.Println("Load Balancer!")
+	var (
+		pool     Pool
+		balancer Balancer
+		requests chan Request = make(chan Request)
+		done     chan *Worker = make(chan *Worker)
+	)
+
+	fmt.Println("Number of CPUs avaiable: ", runtime.NumCPU())
+
+	for i := 0; i < runtime.NumCPU(); i++ {
+		requests := make(chan Request)
+		worker := Worker{requests, 0, i}
+		go worker.Work(done)
+		pool = append(pool, &worker)
+	}
+	heap.Init(&pool)
+
+	balancer = Balancer{pool, done}
+	go balancer.Balance(requests)
+
+	requester(requests)
 }
