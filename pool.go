2e4d43b6e59f8494264e650fcff3e2e0011e6922
Jacky Chiu <jacky.chiu888@gmail.com>
2017-04-04 22:00:19 -0400

    Cleaner way of gettings stats

--- pool.go
+++ pool.go
@@ -50,21 +50,18 @@ func (p *Pool) Pop() interface{} {
 	return elem
 }
 
-func (p Pool) meanAndStdDev() (float64, float64) {
-	var (
-		mean   float64
-		stdDev float64
-	)
+func (p Pool) stats() (mean float64, stdDev float64) {
+	length := float64(len(p))
 
 	for _, worker := range p {
 		mean += float64(worker.pending)
 	}
-	mean /= float64(len(p))
+	mean /= length
 
 	for _, worker := range p {
 		stdDev += math.Pow((float64(worker.pending) - mean), 2)
 	}
-	stdDev = math.Sqrt((1 / float64(len(p)) * stdDev))
+	stdDev = math.Sqrt((1 / length) * stdDev)
 
 	return mean, stdDev
 }
@@ -75,6 +72,6 @@ func (p Pool) String() string {
 		workers += strconv.Itoa(worker.pending) + " "
 	}
 
-	mean, stdDev := p.meanAndStdDev()
+	mean, stdDev := p.stats()
 	return fmt.Sprintf("Workers: %v| Avg Load: %.2f | Std Dev: %.2f", workers, mean, stdDev)
 }
